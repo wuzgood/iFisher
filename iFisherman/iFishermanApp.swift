6cf1f0065c10831a4d02791e102f3bbdc94cbe36
kameller <at1am@icloud.com>
2022-07-04 23:08:45 +0300

    Initial Commit

---
+++ iFisherman/iFishermanApp.swift
@@ -0,0 +1,17 @@
+//
+//  iFishermanApp.swift
+//  iFisherman
+//
+//  Created by Wuz Good on 04.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct iFishermanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
